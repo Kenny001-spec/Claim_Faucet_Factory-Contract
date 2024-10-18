fa97c802d6d7aa115c8193cc51c14671d4c8d0c2
root <root@DESKTOP-NK8P9PO>
2024-10-18 01:32:29 +0100

    Test

--- ignition/modules/ClaimFaucetFactory.ts
+++ ignition/modules/ClaimFaucetFactory.ts
@@ -3,7 +3,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 const ClaimFaucetFactoryModule = buildModule("ClaimFaucetFactoryModule", (m) => {
   const claimfaucetfactory = m.contract("ClaimFaucetFactory");
  
-    return { claimfaucetfactory };
+  return { claimfaucetfactory };
   
 });
 
--- test/ClaimFaucetFactory.ts
+++ test/ClaimFaucetFactory.ts
@@ -2,19 +2,112 @@ import { loadFixture } from "@nomicfoundation/hardhat-toolbox/network-helpers";
 import { expect } from "chai";
 import hre, { ethers } from "hardhat";
 
-describe("Faucet Test", function () {
-    //Reusable async method for deployment
-    async function deployFaucetFixure() {
-      //Contracts are deployed using the first signer/account by default
-  
-      const [owner, addr1] = await hre.ethers.getSigners();
+describe("ClaimFaucetFactory Test", function () {
 
-      const Faucet = await hre.ethers.getContractFactory("Faucet");
-      const faucet = await Faucet.deploy();
+  async function deployClaimFaucetFactoryFixture() {
+    const [deployer, addr1, addr2] = await hre.ethers.getSigners();
 
-      const withdrawalAmount = ethers.parseEther("0.2");
+    const ClaimFaucetFactory = await hre.ethers.getContractFactory(
+      "ClaimFaucetFactory"
+    );
+    const claimFaucetFactory = await ClaimFaucetFactory.deploy();
+
+    return { claimFaucetFactory, deployer, addr1, addr2 };
+  }
+
+  describe("Deployment", function () {
+    it("Should deploy the factory contract", async function () {
+      const { claimFaucetFactory } = await loadFixture(
+        deployClaimFaucetFactoryFixture
+      );
+      expect(await claimFaucetFactory.getLengthOfDeployedContract()).to.equal(0);
+    });
+  });
+
+  it("Should deploy ClaimFaucet and store the contract info", async function () {
+    const { claimFaucetFactory, deployer } = await loadFixture(
+      deployClaimFaucetFactoryFixture
+    );
+    const _name = "VLtToken";
+    const _symbol = "VLT";
+
+    const tx = await claimFaucetFactory.deployClaimFaucet(_name, _symbol);
+    await tx.wait();
+
+    const _index = 0;
+
+    const [deployerAddress, deployedContractAddress] =
+      await claimFaucetFactory.getUserDeployedContractByIndex(_index);
+
+    const allContracts = await claimFaucetFactory.getAllContractDeployed();
+
+    expect(allContracts.length).to.equal(1);
+    expect(allContracts[_index].deployer).to.equal(deployer.address);
+    expect(allContracts[_index].deployedContract).to.equal(
+      deployedContractAddress
+    );
+  });
+
+  describe("getAllContractDeployed", function () {
+    it("Should return all deployed contracts", async function () {
+      const { claimFaucetFactory, addr1, addr2 } = await loadFixture(
+        deployClaimFaucetFactoryFixture
+      );
       
+      await claimFaucetFactory
+        .connect(addr1)
+        .deployClaimFaucet("DLT Token", "DLT");
+      await claimFaucetFactory
+        .connect(addr2)
+        .deployClaimFaucet("ValoraToken", "VLT");
+
+      const allContracts = await claimFaucetFactory.getAllContractDeployed();
+
+      expect(allContracts.length).to.equal(2);
+      expect(allContracts[0].deployer).to.equal(addr1.address);
+      expect(allContracts[1].deployer).to.equal(addr2.address);
+    });
+
+    it("Should not allow zero address to call", async function () {
+      const { claimFaucetFactory } = await loadFixture(
+        deployClaimFaucetFactoryFixture
+      );
   
-      return { faucet, owner, withdrawalAmount, addr1 };
-    }
-})
+      await expect(claimFaucetFactory.getAllContractDeployed()).to.not.be.revertedWith(
+        "Zero not allowed"
+      );
+    });
+  }); 
+
+  describe("getUserDeployedContracts", function () {
+    it("Should return user's deployed contracts", async function () {
+      const { claimFaucetFactory, addr1 } = await loadFixture(
+        deployClaimFaucetFactoryFixture
+      );
+
+      await claimFaucetFactory.connect(addr1).deployClaimFaucet("Deegen Token", "DT");
+      await claimFaucetFactory.connect(addr1).deployClaimFaucet("DLT Token", "DLT");
+
+      const userContracts = await claimFaucetFactory.connect(addr1).getUserDeployedContract();
+      expect(userContracts.length).to.equal(2);
+      expect(userContracts[0].deployer).to.equal(addr1.address);
+      expect(userContracts[1].deployer).to.equal(addr1.address);
+    });
+
+    describe("getUserDeployedContractsByIndex", function () {
+      it("Should return correct contract info by index", async function () {
+        const { claimFaucetFactory, addr1 } = await loadFixture(
+          deployClaimFaucetFactoryFixture
+        );
+  
+        await claimFaucetFactory.connect(addr1).deployClaimFaucet("Deegen Token", "DT");
+        await claimFaucetFactory.connect(addr1).deployClaimFaucet("DLT Token", "DLT");
+  
+        const [deployer] = await claimFaucetFactory
+          .connect(addr1)
+          .getUserDeployedContractByIndex(1);
+        expect(deployer).to.equal(addr1.address);
+      });
+    });
+  });
+});
